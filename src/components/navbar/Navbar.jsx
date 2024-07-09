4b180dec2ee76da76a3b54090973caa3cf0a1e33
macbook <macbook@MacBook-Air-ihajar.local>
2024-07-09 20:46:55 +0100

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import "./navbar.scss";
 
 function Navbar() {
   return (
     <nav>
-        <div className='left'>Left</div>
-        <div className='right'>Right</div>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png " alt="Logo" />
+          <span>Homio</span>
+        </a>
+      </div>
+      <div className="center">
+        <a href="/">Home</a>
+        <a href="/">Agents</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign In</a>
+        <a href="/" className="register">Sign Up</a>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
