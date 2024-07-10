1462127c0d138698ee30d66289406e4f32fe2dd9
macbook <macbook@MacBook-Air-ihajar.local>
2024-07-10 18:07:22 +0100

    Navbar and menu Icon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 function Navbar() {
+  const [open, setOpen] = useState(false)
   return (
     <nav>
       <div className="left">
@@ -18,6 +20,17 @@ function Navbar() {
       <div className="right">
         <a href="/">Sign In</a>
         <a href="/" className="register">Sign Up</a>
+        <div className="menuIcon">
+          <img src="/menu.png" alt="menu" onClick={() => setOpen((prev) => !prev)}/>
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">Agents</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Sign In</a>
+          <a href="/">Sign Up</a>
+        </div>
       </div>
     </nav>
   );
