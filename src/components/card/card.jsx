91ddfd1d1963463fca20c8ab623919593c375a30
macbook <macbook@MacBook-Air-ihajar.local>
2024-07-14 20:57:02 +0100

    List Page

---
+++ src/components/card/card.jsx
@@ -0,0 +1,80 @@
+import { Link } from "react-router-dom"
+import "./card.scss"
+import { CiLocationOn } from "react-icons/ci";
+import { IoBedOutline } from "react-icons/io5";
+import { LuBath } from "react-icons/lu";
+import { GoHeart, GoHeartFill } from "react-icons/go";
+import { AiOutlineMessage, AiFillMessage } from "react-icons/ai";
+import { useState } from "react";
+
+
+
+function Card({ item }) {
+
+  const [isHeartHovered, setIsHeartHovered] = useState(false)
+  const [isMessageHovered, setMessageHovered] = useState(false)
+
+  return (
+    <div className="card">
+      <Link to={`/${item.id}`} className="imageContainer">
+        <img src={item.img} alt="" />
+      </Link>
+      <div className="textContainer">
+        <div className="detailsContainer">
+          <div className="left">
+            <h2 className="title">
+              <Link to={`/${item.id}`}>{item.title}</Link>
+            </h2>
+            <p className="address">
+              <CiLocationOn style={{ color: "#040415", fontSize: "18px" }} />
+              <span>{item.address}</span>
+            </p>
+          </div>
+          <div className="right">
+            <p className="price">
+              <span>${item.price}</span> /m
+            </p>
+            <div className="features">
+              <div className="feature">
+                <IoBedOutline style={{ color: "#040415", fontSize: "18px" }} />
+                <p>{item.bedroom}</p>
+              </div>
+              <hr className="divider" />
+              <div className="feature">
+                <LuBath style={{ color: "#040415", fontSize: "18px" }} />
+                <p>{item.bedroom}</p>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+       
+        <div className="btnContainers">
+          <button
+            onMouseEnter={() => setIsHeartHovered(true)}
+            onMouseLeave={() => setIsHeartHovered(false)}
+          >
+            {isHeartHovered ? (
+              <GoHeartFill style={{ color: "#EC9A29", fontSize: "25px" }} />
+              ): (
+                <GoHeart style={{ color: "#040415", fontSize: "25px" }} />
+              )}
+          </button>
+          <button
+            onMouseEnter={() => setMessageHovered(true)}
+            onMouseLeave={() => setMessageHovered(false)}
+          >
+             {isMessageHovered? (
+              <AiFillMessage style={{ color: "#EC9A29", fontSize: "25px" }} />
+              ): (
+                <AiOutlineMessage style={{ color: "#040415", fontSize: "25px" }} />
+              )}
+            
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/filter/filter.jsx
@@ -0,0 +1,76 @@
+
+import "./filter.scss";
+import { CiLocationOn } from "react-icons/ci";
+import { IoBedOutline } from "react-icons/io5";
+
+function Filter() {
+  // const [textValue, setTextValue] = useState("");
+  // const [numValue, setNumValue] = useState(0);
+
+  return (
+    <div className="filter">
+      <h1>Filter Properties</h1>
+      <div className="top">
+        <div className="itemTop">
+          <CiLocationOn style={{ fontSize: "20px", color: "#666666" }} />
+          <input
+            type="text"
+            name="location"
+            id="location"
+            placeholder="Enter address"
+          />
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="item">
+          <label htmlFor="type">Type</label>
+          <select name="type" id="type">
+            <option value="">Any</option>
+            <option value="rent">Rent</option>
+            <option value="buy">Buy</option>
+            <option value="sell">Sell</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property">Property</label>
+          <select name="property" id="property">
+            <option value="">All</option>
+            <option value="house">House</option>
+            <option value="apartment">Apartment</option>
+            <option value="land">Land</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="priceRange">Price Range $</label>
+          <input
+            className="priceRange"
+            type="range"
+            name="priceRange"
+            id="priceRange"
+            min={0}
+            max={1000000}
+            step={1000}
+            placeholder="Price Range"
+          />
+        </div>
+        <div className="item">
+          <IoBedOutline  style={{ fontSize: "35px", color: "#040415", paddingBottom: "10px" }} />
+          <input
+            type="number"
+            name="bedroom"
+            id="bedroom"
+            min={0}
+            max={10}
+            placeholder="Any"
+          />
+        </div>
+        
+      </div>
+      <button>
+          Search
+        </button>
+    </div>
+  );
+}
+
+export default Filter
