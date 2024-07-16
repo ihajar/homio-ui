a8770295c7485716d68b17d152ce3428b424211e
macbook <macbook@MacBook-Air-ihajar.local>
2024-07-16 10:23:06 +0100

    adding Map component

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -2,6 +2,7 @@ import { listData } from "../../lib/dummydata";
 import "./listPage.scss";
 import Filter from "../../components/filter/filter"
 import Card from "../../components/card/card"
+import Map from "../../components/map/map";
  
 
 function ListPage() {
@@ -12,11 +13,13 @@ function ListPage() {
         <div className="wrapper">
          <Filter />
          {data.map((item) => (
-          <Card key={item.id} item={item} />
+          <Card key={item.id} item={item}  />
          ))}
         </div>
       </div>
-      <div className="mapContainer">Map</div>
+      <div className="mapContainer">
+        <Map items={data} />
+      </div>
     </div>
   )
 }
