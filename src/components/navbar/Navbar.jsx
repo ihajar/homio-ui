29b5e4ff16c63b585f9965fae8296b9a7cd54778
macbook <macbook@MacBook-Air-ihajar.local>
2024-07-20 19:19:04 +0100

    User navbar update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   const [open, setOpen] = useState(false)
+  const user = true;
+
   return (
     <nav>
       <div className="left">
@@ -20,18 +22,55 @@ function Navbar() {
         <Link to="/">Contact</Link>
       </div>
       <div className="right">
-        <Link to="/">Sign In</Link>
-        <Link to="/" className="register">Sign Up</Link>
+        {user ? (
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+              <span>John Doe</span>
+            </div>
+            <button>
+              <Link to="/profile">
+                <div className="notification">3</div>
+                <span>Profile</span>
+              </Link>
+            </button>
+          </div>
+        ) : (
+          <>
+            <Link to="/">Sign In</Link>
+            <Link to="/" className="register">
+              Sign Up
+            </Link>
+          </>
+        )}
         <div className="menuIcon">
-          <img src="/menu.png" alt="menu" onClick={() => setOpen((prev) => !prev)}/>
+          <img
+            src="/menu.png"
+            alt="menu"
+            onClick={() => setOpen((prev) => !prev)}
+          />
         </div>
         <div className={open ? "menu active" : "menu"}>
           <Link to="/">Home</Link>
           <Link to="/">Agents</Link>
           <Link to="/">About</Link>
           <Link to="/">Contact</Link>
-          <Link to="/">Sign In</Link>
-          <Link to="/">Sign Up</Link>
+          {user ? (
+            <button>
+              <Link to="/profile">
+                <div className="notification">3</div>
+                <span>Profile</span>
+              </Link>
+            </button>
+          ) : (
+            <>
+              <Link to="/">Sign In</Link>
+              <Link to="/">Sign Up</Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
---
+++ src/routes/profilePage/profilePage.jsx
@@ -0,0 +1,11 @@
+import "./profilePage.scss";
+
+
+
+function ProfilePage() {
+  return (
+    <div className='profilePage'>ProfilePage</div>
+  )
+}
+
+export default ProfilePage
